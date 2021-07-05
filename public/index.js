73f27dba0b31bc39d1d85cede6d650b6f19120f4
mizdra <pp.mizdra@gmail.com>
2021-07-06 00:05:57 +0900

    update

--- public/index.js
+++ public/index.js
@@ -1,3 +1,8 @@
+Sentry.init({
+  dsn: 'https://75d67850fa86409aa16044c052c425be@o912199.ingest.sentry.io/5849017',
+  debug: true,
+});
+
 const ROUTES = [
   { path: '/a', entrypoint: '/a.js' },
   { path: '/b', entrypoint: '/b.js' },
@@ -5,16 +10,10 @@ const ROUTES = [
   { path: '/d', entrypoint: '/d.js' },
 ];
 
-Sentry.init({
-  dsn: 'https://75d67850fa86409aa16044c052c425be@o912199.ingest.sentry.io/5849017',
-});
-
 const params = new URLSearchParams(new URL(location).search);
 const path = params.get('path');
 
 const route = ROUTES.find((route) => route.path === path);
 if (route) {
-  import(route.entrypoint)
-    .then(({ entrypoint }) => entrypoint())
-    .catch(console.error);
+  import(route.entrypoint).then(({ entrypoint }) => entrypoint());
 }
